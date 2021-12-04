a5e42a7a11c86f5ac0dddca931aa45dc3ab919f9
Michael Currie <michaelalancurrie@gmail.com>
2021-12-04 08:29:38 -0800

    update faq component

--- client/src/components/FaqComponent.jsx
+++ client/src/components/FaqComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import data from "../data";
+import { data } from "../data";
 import { useState } from "react";
-
+import "./FaqComponent.scss";
 export default function FaqComponent() {
   const [selected, setSelected] = useState(null);
 
@@ -17,18 +17,20 @@ export default function FaqComponent() {
     <>
       <div className="faq">
         <div className="faq__accordian">
-          {data.map((item, i) => {
+          {data.map((item, i) => (
             <div className="faq__item">
               <div className="faq__title" onClick={() => toggle(i)}>
                 <h2>{item.question}</h2>
                 <span>{selected === i ? "-" : "+"}</span>
               </div>
 
-              <div className={selected === i ? "answer-show" : "answer"}>
+              <div
+                className={selected === i ? "faq__answer-show" : "faq__answer"}
+              >
                 {item.answer}
               </div>
-            </div>;
-          })}
+            </div>
+          ))}
         </div>
       </div>
     </>
